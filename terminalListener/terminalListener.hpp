207928e7624b4267cf51fcb1af81b94c3c1fcffd
Krzysztof Prus <krprus@gmail.com>
2020-07-31 21:29:11 +0200

    typo

--- terminalListener/terminalListener.hpp
+++ terminalListener/terminalListener.hpp
@@ -8,7 +8,7 @@ class TerminalListener : public Receiver
 public:
     TerminalListener(Sender* sender, Components* components, QObject *parent = nullptr, quint16 port = 12345);
 
-    void handleMessage(Message &msg) override;
+    void handleMessage(QNetworkDatagram msg) override;
 private:
     Components* mComponents;
     std::mutex mMutex;
